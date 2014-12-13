7b66dc4dc0c6570945ed34ff188ab0cf404b0804
marcin walendzik <marcin.walendzik@gmail.com>
2014-12-13 18:17:57 +0000

    initial set up

--- lib/atc.rb
+++ lib/atc.rb
@@ -6,16 +6,27 @@ class ATC
       @x = args[:gridx]
       @y = args[:gridy]
       @airports = {}
-      @planes=[]
+      @planes={}
   end  
 
   #adding airports to the system
-  def add_airport(airport,airport_id)
-    @airports[airport_id]=airport
+  def add_airport(airport,airport_id,x,y)
+    @airports << {airport: airport,id: id,x: x, y: y}
   end  
 
   #adding plane to the space 
   def add_planes(plane)
-    @planes << plane
+    @planes << {plane: plane, x: random(@x), y: random(@y)}
+  end
+
+
+  #counts number of planes in the system
+  def planes_n
+    @planes.count
+  end
+
+  #counts number of airports in the system
+  def airports_n
+    @airports.count
   end
 end
--- spec/atc_spec.rb
+++ spec/atc_spec.rb
@@ -2,3 +2,18 @@ require 'atc'
 require 'plane'
 require 'airport'
 
+describe ATC do
+  
+  let(:luton){Airport.new(airport_id: 1,capacity: 5)}
+  let(:stanstead){Airport.new(airport_id: 2,capacity: 5)}
+
+  let(:planes1){5.times.map{Plane.new(airport_id: 1)}}
+  let(:planes2){5.times.map{Plane.new(airport_id: 2)}}
+
+  let(:atc){ATC.new(gridx: 10, gridy: 10)}
+
+  it 'should have no planes initially' do
+    expect(atc.planes_n).to eq(0)
+  end
+
+end
